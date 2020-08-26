3329424095d60dcbb020a8775c612a3ca55b67cb
Allan Yong <allanyong@Allans-iMac-Pro.local>
2020-08-26 11:15:39 -0400

    updated the default directory setting to windows

--- applicator.py
+++ applicator.py
@@ -2,13 +2,17 @@ import filetype
 import os
 from zipfile import ZipFile, ZipInfo
 from io import BytesIO
+import codecs
 import json
 import xml.dom.minidom as md
 
 
+dirName = os.getcwd()
+
+print(dirName)
 valuesToRemove = ['adapt-contrib-languagePicker', 'adapt-pagesInNavBar', 'adapt-close', 'adapt-devtools']
 keysToRemove = ['_close', '_pagesInNavBar','_languagePicker']
-dirName = os.getcwd()
+
 
 def getListOfFiles(dirName):
     # create a list of file and sub directories
@@ -19,13 +23,63 @@ def getListOfFiles(dirName):
     for entry in listOfFile:
         # Create full path
         fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
+        # If it is a path, ignore
+        if os.path.isdir(fullPath) is not True:
             allFiles.append(fullPath)
 
     return allFiles
+def updateFRlabel(contentData,destJson):
+    for (key, value) in contentData.items():
+        if type(value) == type(dict()):
+            destJson.update(value)
+            updateFRlabel(value,destJson)
+    #print(json.dumps(destJson,indent=2,separators=(',', ': '),ensure_ascii=False))
+    return destJson
+def updateFRCourseJson(course):
+
+    with open('courseContent.json',encoding='utf8') as f:
+        contentData = json.load(f)
+    #print(json.dumps(json.loads(course),separators=(',', ': '),indent=2))
+    data = updateFRlabel(contentData,json.loads(course))
+    
+    return json.dumps(data,indent=2,separators=(',', ': '),ensure_ascii=False)
+def updateCourseJson(course):
+
+    ##################### Load the json files ############################
+
+    data = json.loads(course)
+
+    ################# Remove keys/values from the course.json file #####################
+    for key in keysToRemove:
+        while True:
+            try:
+                del data[key]
+                del data['_globals']['_extensions'][key]
+            except KeyError as e:
+                print(e)
+                break
+
+    ###################### Patch the course.json file ##################################
+    
+    data['_pageLevelProgress']['_isEnabled'] = True
+    data['_pageLevelProgress']['_showPageCompletion'] = True
+    data['_pageLevelProgress']['_isCompletionIndicatorEnabled'] = False
+    data['_pageLevelProgress']['_isShownInNavigationBar'] = False
+    data['_resources']['_isEnabled'] = True
+    data['_glossary']['_isEnabled'] = True
+    data['_bookmarking']['_isEnabled'] = True
+    data['_bookmarking']['_showPrompt'] = True
+    data['_pageIncompletePrompt']['_isEnabled'] = True
+    data['_homeButton']['_isEnabled'] = True
+
+
+
+
+    print('End of updateCourseJson Function, returning the json object...')
+    ################# return it to a json data object #####################
+    
+    
+    return json.dumps(data,indent=2,separators=(',', ': '),ensure_ascii=False)
 def updateConfigJson(config, language):
     data = json.loads(config)
     ####################################################################################
@@ -63,57 +117,9 @@ def updateConfigJson(config, language):
     data['_completionCriteria']['_requireAssessmentCompleted'] = False
     '''
     #print(json.dumps(data['_defaultLanguage'], indent=4, sort_keys=True))
-    return json.dumps(data)
+    return json.dumps(data,indent=2,separators=(',', ': '),ensure_ascii=False)
     #print(json.dumps(data['build']['includes'], indent=4, sort_keys=True))
     #################################################################################    
-def updateCourseJson(course):
-
-    data = json.loads(course)
-    ####################################################################################
-    ################# Remove keys/values from the course.json file #####################
-    ####################################################################################
-    for key in keysToRemove:
-        while True:
-            try:
-                del data[key]
-                del data['_globals']['_extensions'][key]
-                #data.remove(key)
-            except KeyError as e:
-                print(e)
-                break
-
-    ####################################################################################
-    ###################### Patch the course.json file ##################################
-    ####################################################################################
-    
-    data['_pageLevelProgress']['_isEnabled'] = True
-    data['_pageLevelProgress']['_showPageCompletion'] = True
-    data['_pageLevelProgress']['_isCompletionIndicatorEnabled'] = False
-    data['_pageLevelProgress']['_isShownInNavigationBar'] = False
-    data['_resources']['_isEnabled'] = True
-    data['_glossary']['_isEnabled'] = True
-    data['_bookmarking']['_isEnabled'] = True
-    data['_bookmarking']['_showPrompt'] = True
-    data['_pageIncompletePrompt']['_isEnabled'] = True
-    data['_homeButton']['_isEnabled'] = True
-    
-    '''
-    #debug information
-    data['_pageLevelProgress']['_isEnabled'] = False
-    data['_pageLevelProgress']['_showPageCompletion'] = False
-    data['_pageLevelProgress']['_isCompletionIndicatorEnabled'] = False
-    data['_pageLevelProgress']['_isShownInNavigationBar'] = False
-    data['_resources']['_isEnabled'] = False
-    data['_glossary']['_isEnabled'] = False
-    data['_bookmarking']['_isEnabled'] = False
-    data['_bookmarking']['_showPrompt'] = False
-    data['_pageIncompletePrompt']['_isEnabled'] = False
-    data['_homeButton']['_isEnabled'] = False
-    '''
-    print('*******************************************')
-    #print(json.dumps(data['title'], indent=2, sort_keys=True))
-
-    return json.dumps(data)
 def updateimsXML(file):
     dom = md.parseString(file)
 
@@ -123,90 +129,80 @@ def updateimsXML(file):
     #print(dom.getElementsByTagName("file")[0].getAttribute("href"))
     #print(dom.toxml())
     return dom.toprettyxml()
-    
+def isdir(z, name):
+    return any(x.startswith("%s/" % name.rstrip("/")) for x in z.namelist())
 ######################## Update the zip file ###################################
 def update_or_insert(path):
     configJSON = None
     courseJSON = None
+    frenchLabel = None
+    contentData = None
     imsXML = None
     courseLan = None
-    launchFile = None
-
-    
-    """
-    Param: path -> file in archive
-    Param: data -> data to be updated
-
-    Returns a new zip file with the updated content
-    for the given path
-    """
     new_zip = BytesIO()
+
 
     with ZipFile(path, 'r') as zip_archive:
         with ZipFile(new_zip, 'w') as new_archive:
             ### add HTMl file into the ZIP ###
-            new_archive.write('launch.html')
+            new_archive.write('/Users/allanyong/jsonPatcher/launch.html')
             ### determine the language of the course package ###
-            if path[-6:] == 'fr.zip':
+            if path[-6:] == 'fr.zip' or path[-6:] == 'FR.zip':
                 courseLan = 'fr'
             else:
                 courseLan = 'en'
+            print(courseLan)   
             ### Patch the json and xml files ###
             for file in zip_archive.filelist:
                 # If you spot an existing file, create a new object
-                if file.filename == 'course/en/course.json':
-                    courseJSON = updateCourseJson(zip_archive.read(file))
-                    zip_inf = ZipInfo(file.filename)
-                    new_archive.writestr(zip_inf, courseJSON)   
-                elif file.filename == 'course/config.json':
-                    configJSON = updateConfigJson(zip_archive.read(file),courseLan)
-                    zip_inf = ZipInfo(file.filename)
-                    new_archive.writestr(zip_inf, configJSON)
-                elif file.filename == 'imsmanifest.xml':
-                    imsXML = updateimsXML(zip_archive.read(file))
-                    zip_inf = ZipInfo(file.filename)
-                    new_archive.writestr(zip_inf, imsXML)
-                else:
-                    # Copy other contents as it is
-                    new_archive.writestr(file, zip_archive.read(file.filename))  
+                #print(file.filename + '====================================')
+                if isdir(zip_archive, "course/"+courseLan):
+                    if file.filename == 'course/'+courseLan+'/course.json':
+                        print("I found a file: " + file.filename)
+                        courseJSON = updateCourseJson(zip_archive.read(file))
+                        if courseLan == 'fr':
+                            courseJSON = updateFRCourseJson(courseJSON)
+                        zip_inf = ZipInfo(file.filename)
+                        new_archive.writestr(zip_inf, courseJSON)   
+                    elif file.filename == 'course/config.json':
+                        print("I found a file: " + file.filename)
+                        configJSON = updateConfigJson(zip_archive.read(file),courseLan)
+                        zip_inf = ZipInfo(file.filename)
+                        new_archive.writestr(zip_inf, configJSON)
+                    elif file.filename == 'imsmanifest.xml':
+                        print("I found a file: " + file.filename)                    
+                        imsXML = updateimsXML(zip_archive.read(file))
+                        zip_inf = ZipInfo(file.filename)
+                        new_archive.writestr(zip_inf, imsXML)
+                    else:
+                        # Copy other contents as it is
+                        new_archive.writestr(file, zip_archive.read(file.filename))  
 
     return new_zip
-######################## Main Logic (please switch to the correct OS in this block) ###################################
 def main():
     numOfZip = 0
     # Get the list of all files in directory tree at given path
     listOfFiles = getListOfFiles(dirName)
-
     for elem in listOfFiles:
         kind = filetype.guess(elem)
         if not (kind is None):
             if kind.mime == 'application/zip' and kind.extension == 'zip':
-                '''
-                # DEBUG: check to see what ZIP files are found
-                # print(elem)
-                # print(kind.extension)
-                # print(kind.mime)
-                # Debug: Number of Zip Files found
-                '''
                 numOfZip += 1
                 print('Found %s Zip Files' % numOfZip)
+                print(elem)
                 ############################################
                 # Patching the zip files #
-                if elem[-16:] != 'base_library.zip':
-                    new_zip = update_or_insert(elem)
-                    print(elem)
+                new_zip = update_or_insert(elem)
                 #Generate new Zips#
-                
-                    try:
-                        #for mac 
-                        with open('Patched - ' + elem.replace(dirName+'/',''), 'wb') as f:
-                        #for windows
-                        #with open('Patched - ' + elem.replace(dirName+'\\',''), 'wb') as f:
-                            f.write(new_zip.getbuffer())
-                            new_zip.close()
-                    except OSError as e:
-                        print(e)
-                
-                
+                try:
+                    #for mac 
+                    with open('Patched - ' + elem.replace(dirName+'/',''), 'wb') as f:
+                    #for windows
+                    #with open('Patched - ' + elem.replace(dirName+'\\',''), 'wb') as f:
+                        f.write(new_zip.getbuffer())
+                        new_zip.close()
+                except OSError as e:
+                    print(e)
+
 if __name__ == '__main__':
     main()
